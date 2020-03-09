eb3eeb77e035dd17a0b1744740aa53719029d8c9
Clinton N. Dreisbach <clinton@dreisbach.us>
2020-03-09 11:15:09 -0400

    Update settings.py

--- project_name/settings.py
+++ project_name/settings.py
@@ -68,7 +68,7 @@ ROOT_URLCONF = '{{ project_name }}.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': BASE_DIR / 'templates',
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
